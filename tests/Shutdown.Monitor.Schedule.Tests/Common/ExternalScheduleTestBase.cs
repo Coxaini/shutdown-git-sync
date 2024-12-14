ce9828fec4e4a9a3cf73888a58ba1497d7bc836b
Ilya Afanasiev <ilya.afanasiev@swissphone.com>
2024-12-14 23:16:37 +0200

    Refactor configuration of the schedule tests

---
+++ tests/Shutdown.Monitor.Schedule.Tests/Common/ExternalScheduleTestBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Shutdown.Monitor.Schedule.Tests.Common;
+
+public abstract class ExternalScheduleTestBase
+{
+    protected readonly ExternalScheduleConfig Configuration;
+
+    protected ExternalScheduleTestBase()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("testsettings.json")
+            .AddUserSecrets<ExternalScheduleConfig>()
+            .Build();
+
+        var config = configuration.Get<ExternalScheduleConfig>();
+
+        Configuration = config ?? throw new ArgumentNullException(nameof(ExternalScheduleConfig));
+    }
+}
--- tests/Shutdown.Monitor.Schedule.Tests/YasnoShutDownSiteParserTests.cs
+++ tests/Shutdown.Monitor.Schedule.Tests/YasnoShutDownSiteParserTests.cs
@@ -3,30 +3,31 @@ using Shutdown.Monitor.Schedule.Common.Configs;
 using Shutdown.Monitor.Schedule.Parsing;
 using Shutdown.Monitor.Schedule.Parsing.Interfaces;
 using Shutdown.Monitor.Schedule.Parsing.Strategies;
+using Shutdown.Monitor.Schedule.Tests.Common;
 
 namespace Shutdown.Monitor.Schedule.Tests;
 
-public class YasnoShutDownSiteSeleniumParserTests
+public class YasnoShutDownSiteSeleniumParserTests : ExternalScheduleTestBase
 {
     private readonly IShutDownSiteParser _parser;
+
     public YasnoShutDownSiteSeleniumParserTests()
     {
-        var options =Options.Create(new ShutDownServiceConfig()
+        var options = Options.Create(new ShutDownServiceConfig()
         {
-            SiteUrl = "https://kyiv.yasno.com.ua/schedule-turn-off-electricity"
+            SiteUrl = Configuration.SiteUrl
         });
         _parser = new SeleniumShutDownPageParser(new YasnoSeleniumShutDownPageParsingStrategy(), options);
     }
-    
+
     [Fact]
     public async Task RetrieveGroupScheduleAsync_ShouldReturnGroupSchedule()
     {
         var date = DateOnly.FromDateTime(DateTime.Now);
-        
+
         var result = await _parser.RetrieveGroupScheduleAsync(date);
-        
+
         Assert.NotNull(result);
         Assert.NotEmpty(result);
     }
-    
 }
