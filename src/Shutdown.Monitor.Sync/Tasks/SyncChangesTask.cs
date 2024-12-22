7b14f0016955aa5e9c244500fd095f6ff69f5c86
Ilya <ilyaafon5@gmail.com>
2024-12-22 16:35:41 +0200

    Add sync changes task

--- src/Shutdown.Monitor.Sync/Tasks/SyncChangesTask.cs
+++ src/Shutdown.Monitor.Sync/Tasks/SyncChangesTask.cs
@@ -1,11 +1,37 @@
 ﻿using Quartz;
+using Shutdown.Monitor.Git.Interfaces;
+using Shutdown.Monitor.Sync.Interfaces;
+using Shutdown.Monitor.Sync.Models;
 
 namespace Shutdown.Monitor.Sync.Tasks;
 
 public class SyncChangesTask : IJob
 {
-    public Task Execute(IJobExecutionContext context)
+    private readonly IGitChangesSender _gitChangesSender;
+    private readonly ITriggerSyncHttpClient _triggerSyncHttpClient;
+    private readonly ILogger<SyncChangesTask> _logger;
+
+    public SyncChangesTask(IGitChangesSender gitChangesSender, ITriggerSyncHttpClient triggerSyncHttpClient,
+        ILogger<SyncChangesTask> logger)
     {
-        return Task.CompletedTask;
+        _gitChangesSender = gitChangesSender;
+        _triggerSyncHttpClient = triggerSyncHttpClient;
+        _logger = logger;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var gitResult = _gitChangesSender.SaveChangesToTempBranch();
+        if (gitResult.IsFailure)
+        {
+            _logger.LogError("Failed to save changes to temp branch. Error: {Error}", gitResult.Exception);
+            return;
+        }
+
+        var value = gitResult.Value;
+        var syncRequest = new SyncRequest(value.LastBranchName, value.TempBranchName,
+            value.CommitedFiles.Select(f => new SyncFile(f.FilePath, f.WasUntracked)));
+
+        await _triggerSyncHttpClient.TriggerSync(syncRequest);
     }
 }
